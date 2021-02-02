b2ae2f23a1ea98b866f2d852bc732e02e3463fb7
Luis Roberto Oliveira Santos <TNEP00114@equatorial.corp>
2021-02-02 09:33:30 -0300

    creating the signIn page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SignIn: React.FC = () => <h1>Sign In</h1>;
+
+export default SignIn;
---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #E5E5E5;
+    color: #444444;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
